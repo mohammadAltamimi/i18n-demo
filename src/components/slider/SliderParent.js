4124dc9c125214793cc4856cffb29252640a1806
TahaAlTamimi <altamimitmo@gmail.com>
2020-04-25 00:41:58 +0300

    last editin git hub

--- src/components/slider/SliderParent.js
+++ src/components/slider/SliderParent.js
@@ -5,33 +5,33 @@ import { useTranslation } from "react-i18next";
 function SliderParent (){
     const { t, i18n } = useTranslation();
     // console.log(' slider',i18n.language  )
-    // console.log('data',(t('data',{ returnObjects: true })))
-        // let data = [
-        //           'Apple',
-        //           'Ball',
-        //           'Cat',
-        //           'Dog',
-        //           'Elephant',
-        //           'Fruits',
-        //           'Gorilla',
-        //           'Horse',
-        //           'Ink',
-        //           'Jug',
-        //           'Kite',
-        //           'Lemon',
-        //           'Orange',
-        //           'Paddy',
-        //           'Queen',
-        //           'Rose',
-        //           'Street',
-        //           'Tuesday',
-        //           'Umbrella',
-        //           'Vanilla',
-        //           'Wax',
-        //           'Xerox',
-        //           'Yarn',
-        //           'Zebra'
-        //       ];
+    console.log('data',(t('data',{ returnObjects: true })))
+        let data = [
+                  'Apple',
+                  'Ball',
+                  'Cat',
+                  'Dog',
+                  'Elephant',
+                  'Fruits',
+                  'Gorilla',
+                  'Horse',
+                  'Ink',
+                  'Jug',
+                  'Kite',
+                  'Lemon',
+                  'Orange',
+                  'Paddy',
+                  'Queen',
+                  'Rose',
+                  'Street',
+                  'Tuesday',
+                  'Umbrella',
+                  'Vanilla',
+                  'Wax',
+                  'Xerox',
+                  'Yarn',
+                  'Zebra'
+              ];
               return (
                   <div className={`parent ${i18n.language}`}  >
                       <Slider lang={i18n.language}>
