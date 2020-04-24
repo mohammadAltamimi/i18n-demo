b0b7df1d8a3f1ab9a1c6312464dfb70344fb201c
TahaAlTamimi <altamimitmo@gmail.com>
2020-04-25 00:15:52 +0300

    Updates

---
+++ precache-manifest.6f983fa3d9a246347ea0c6db168ac062.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66e47ac2b36f4b08609cf3f31c517816",
+    "url": "/i18n-demo/index.html"
+  },
+  {
+    "revision": "27449be6ca4e3fbf5918",
+    "url": "/i18n-demo/static/css/main.dcfb0e20.chunk.css"
+  },
+  {
+    "revision": "088a01d6bb1e76a9eaa8",
+    "url": "/i18n-demo/static/js/2.16ae313e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/i18n-demo/static/js/2.16ae313e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27449be6ca4e3fbf5918",
+    "url": "/i18n-demo/static/js/main.108b6541.chunk.js"
+  },
+  {
+    "revision": "3c51e51e82c460bbaa06",
+    "url": "/i18n-demo/static/js/runtime-main.a26cb763.js"
+  },
+  {
+    "revision": "5e98861f6ef1ce225121abfb3f418166",
+    "url": "/i18n-demo/static/media/getFetch.5e98861f.cjs"
+  }
+]);
